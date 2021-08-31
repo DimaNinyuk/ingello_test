98d3dd4e92a636ffef60a6089961d09bbe12ed4a
DimaNinyuk <34214183+DimaNinyuk@users.noreply.github.com>
2021-08-31 14:10:03 +0300

    time4

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
  
-$fn = ($p = key($_GET)) ? 'content/' . $p . '/index.php' : 'content/index.php';
  
-(file_exists($fn)) ? require $fn : require 'content/error404.php';
+$fn = (key($_GET)) ? '/' : 'content/index.php';
+$fn = (key($_GET)) ? '/user/auth/create-token' : 'content/user/AuthController.php';
+$fn = (key($_GET)) ? 'content/' : 'content/index.php';
+$fn = (key($_GET)) ? 'content/': 'content/index.php';
+ 
+(file_exists($fn)) ? require $fn : require 'content/error404.php' ;
+(file_exists($fn)) ? require $fn : require 'content/error404.php' ;
+(file_exists($fn)) ? require $fn : require 'content/error404.php' ;
+(file_exists($fn)) ? require $fn : require 'content/error404.php' ;
+
 ?>
