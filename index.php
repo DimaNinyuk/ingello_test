24ba5d267440554bf31c8e9f37cd865a8c626466
DimaNinyuk <34214183+DimaNinyuk@users.noreply.github.com>
2021-08-31 15:34:38 +0300

    finish

--- index.php
+++ index.php
@@ -1,35 +1,33 @@
 <?php
- include('content/SiteController.php');
- include('content/user/AuthController.php');
- include('content/store/ProductController.php');
- $p = key($_GET);
+include('content/SiteController.php');
+include('content/user/AuthController.php');
+include('content/store/ProductController.php');
+include('content/cart/CartController.php');
+$p = key($_GET);
 //$fn = ($p = key($_GET)) ? 'content/' . $p . '/index.php' : 'content/index.php';
 //$fn2 = ($p = key($_GET)) ? 'site/contact/' : 'content/SiteController.php';
- if($p == "site/contact"){
-    $obj = new  SiteController();
-    $obj->contact();
-
- }
- else if($p == ""){
-    require_once('content/index.php');
- }
- else if($p == "user/auth/create-token"){
-    $obj = new AuthController();
-    $obj->createToken();
- }
- else if($p == "store/product/create"){
-    $obj = new ProductController();
-    $obj->create();
- }
- else if($p =="store/product"){
-    $obj = new ProductController();
-    $date = $_GET["date"];
-    $obj->index($date);
- }
- else{
-     require_once("content/error404.php");
- }
+if ($p == "site/contact") {
+   $obj = new  SiteController();
+   $obj->contact();
+} else if ($p == "") {
+   require_once('content/index.php');
+} else if ($p == "user/auth/create-token") {
+   $obj = new AuthController();
+   $obj->createToken();
+} else if ($p == "store/product/create") {
+   $obj = new ProductController();
+   $obj->create();
+} else if ($p == "store/product") {
+   $obj = new ProductController();
+   $date = $_GET["date"];
+   $obj->index($date);
+} else if ($p == "cart/add") {
+   $obj = new CartController();
+   $obj->addToCart();
+} else if ($p == "cart/delete") {
+   $obj = new CartController();
+   $obj->delreFromCart();
+} else {
+   require_once("content/error404.php");
+}
 //(file_exists($fn2)) ? require $fn2 : require 'content/error404.php'; 
-
-
-?>
