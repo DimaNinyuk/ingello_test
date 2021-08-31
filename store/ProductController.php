a990d0a35d8fb7239f62d2ff23303d4b638e6334
DimaNinyuk <34214183+DimaNinyuk@users.noreply.github.com>
2021-08-31 13:39:36 +0300

    time 2

--- store/ProductController.php
+++ store/ProductController.php
@@ -1,5 +1,13 @@
+<?php 
 class ProductController{
-    function create($id){
-        return echo $id;
+    function create(){
+        $id = $_POST["id"];
+        $name = $_POST["name"];
+        $price = $_POST["price"];
+        echo $id + $name + $price ;
+    }
+    function index($date){
+        echo $date;
     }
 }
+?>
