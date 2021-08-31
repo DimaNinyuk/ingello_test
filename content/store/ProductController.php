45346c4dade4bfeae7244ecde98849e1580bbbe5
DimaNinyuk <34214183+DimaNinyuk@users.noreply.github.com>
2021-08-31 15:12:53 +0300

    time8

--- content/store/ProductController.php
+++ content/store/ProductController.php
@@ -1,13 +1,15 @@
-<?php 
-class ProductController{
-    function create(){
+<?php
+class ProductController
+{
+    function create()
+    {
         $id = $_POST["id"];
         $name = $_POST["name"];
         $price = $_POST["price"];
-        echo $id + $name + $price ;
+        echo $id . $name . $price;
     }
-    function index($date){
+    function index($date)
+    {
         echo $date;
     }
 }
-?>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
  include('content/SiteController.php');
  include('content/user/AuthController.php');
- include('content/store/ProductController.php')
+ include('content/store/ProductController.php');
  $p = key($_GET);
 //$fn = ($p = key($_GET)) ? 'content/' . $p . '/index.php' : 'content/index.php';
 //$fn2 = ($p = key($_GET)) ? 'site/contact/' : 'content/SiteController.php';
@@ -13,13 +13,21 @@
  else if($p == ""){
     require_once('content/index.php');
  }
- else if($p = "user/auth/create-token"){
+ else if($p == "user/auth/create-token"){
     $obj = new AuthController();
     $obj->createToken();
  }
- else if($p = "/store/product/create"){
+ else if($p == "store/product/create"){
     $obj = new ProductController();
-    
+    $obj->create();
+ }
+ else if($p =="store/product"){
+    $obj = new ProductController();
+    $date = $_GET["date"];
+    $obj->create($date);
+ }
+ else{
+     require_once("content/error404.php");
  }
 //(file_exists($fn2)) ? require $fn2 : require 'content/error404.php'; 
 
