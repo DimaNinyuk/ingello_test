12574429cd7c8d4ac1f012f8050de44d3544c651
DimaNinyuk <34214183+DimaNinyuk@users.noreply.github.com>
2021-08-31 14:40:43 +0300

    time 6

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
  
 
 $fn = ($p = key($_GET)) ? 'content/' . $p . '/index.php' : 'content/index.php';
-$fn2 = ($p = key($_GET)) ? 'site/contact/' . $p . '/SiteController.php' : 'content/SiteController.php';
+$fn2 = ($p = key($_GET)) ? 'site/contact/' : 'content/SiteController.php';
  
-(file_exists($fn)) ? require $fn : require 'content/error404.php'; 
+(file_exists($fn2)) ? require $fn2 : require 'content/error404.php'; 
 
 
 ?>
